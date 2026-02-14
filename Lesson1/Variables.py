ca390ce0bf53dae3e8f5eb1e7a16878abb00d084
GermaniumMo <morinagermanium@gmail.com>
2026-02-14 16:49:30 +0100

    add

---
+++ Lesson1/Variables.py
@@ -0,0 +1,25 @@
+from tkinter.scrolledtext import example
+
+temperature = 98.5
+name = "John"
+
+print(type(temperature))
+print(name)
+
+x = 5
+y = 3
+result = x + y
+print(result)
+
+first_name = "John"
+last_name = "Doe"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+name = ["Item1", "Item2", "Item3"]
+
+print(name[1])
+
+test = ["Item1", "Item2", 3, 4.5]
+print(test[3])
