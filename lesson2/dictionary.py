202c914d78bf419919a869681c4a32114658ef56
GermaniumMo <morinagermanium@gmail.com>
2026-02-21 16:32:42 +0100

    add

---
+++ lesson2/dictionary.py
@@ -0,0 +1,9 @@
+contact_info = {
+    "Alice": "555-1234",
+    "Bob": "555-6547"
+}
+
+print(contact_info["Alice"])
+print(contact_info)
+print(contact_info.values())
+print(contact_info.keys())
---
+++ lesson2/tuples_dictionaries.py
@@ -0,0 +1,10 @@
+grades = {
+    ("John", "Math"): 5,
+    ("Alice", "Biology"): 4,
+    ("Bob", "Physics"): 3.5,
+    ("Eve", "Music"): 5,
+    ("John", "English"): 4
+}
+
+grades.copy()
+print(grades)
