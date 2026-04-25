a21b4f064287e6e084b2fbcdc9c4a45539e07906
GermaniumMo <morinagermanium@gmail.com>
2026-04-25 17:08:45 +0200

    add

--- lesson11/file_operations.py
+++ lesson11/file_operations.py
@@ -23,5 +23,22 @@ lines = ["Amiri edhe Noart shkrujn shum shpejt\n", "Erjoni edhe Germanium hajn s
 with open("example", 'w') as file:
     file.writelines(lines)
 
+with open("example", 'r') as file:
+    file.seek(0)
+    data = file.read()
+    print(data)
 
 
+if os.path.exists('example'):
+    print("File exists")
+
+with open('example', 'a') as file:
+    file.write('\nNew data appended.')
+
+data = b'This is some binary data'
+with open('example.bin', 'wb') as file:
+    file.write(data)
+
+with open('example.bin', 'rb') as binary_file:
+    data = binary_file.read()
+    print(data)
